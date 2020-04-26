1899215c466f5d97382a9d79dbdb06472f18a6dc
niccholaspage <niccholaspage@gmail.com>
2020-04-26 18:29:08 -0400

    Handle program arguments properly, accepting only one

--- SPAN/Main.cpp
+++ SPAN/Main.cpp
@@ -5,6 +5,15 @@ using namespace std;
 int main()
 {
 	cout << "test print\n";
+	{
+		cout << "Wrong number of arguments provided. Example usage:\n";
+		cout << "SPAN graph_file_path\n";
+
+		return 0;
+	}
+
+	cout << "argv[0]: " << argv[0] << "\n";
+	cout << "argv[1]: " << argv[1] << "\n";
 
 	return 0;
 }
