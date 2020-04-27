5c6ff58446720226b26b3f8bc96f008f9fd513d2
niccholaspage <niccholaspage@gmail.com>
2020-04-27 18:39:30 -0400

    Work on comments, and change Kruskal object to a pointer

--- SPAN/Main.cpp
+++ SPAN/Main.cpp
@@ -28,13 +28,32 @@ void parseGraphFile(const string& file_path)
 		inputStream >> nodeVertices[i];
 	}
 
+	// We need a 2D array containing our weights / adjacency
+	// matrix. To do this, we create an array of double
+	// pointers, with each pointer of this array pointing
+	// to the first double of each row of the matrix.
+	// The new keyword will then return a pointer to the
+	// first pointer in our adjacency matrix, meaning
+	// the item at the first row and column of the
+	// matrix.
 	double** weights = new double* [numberOfNodes];
 
+	// We now need to loop through our number of nodes,
+	// and initialize each row of the adjacency matrix
+	// to a new double array with a size of number of nodes.
+	// When this loop finishes executing, we will successfully
+	// created our matrix, but have not initialized its
+	// values.
 	for (unsigned int i = 0; i < numberOfNodes; i++)
 	{
 		weights[i] = new double[numberOfNodes];
 	}
 
+	// We now loop through each row, and then
+	// each column of the matrix, reading the input
+	// stream for the weight, and piping it into our
+	// adjacency matrix. After these two loops, our
+	// adjacency matrix is complete.
 	for (unsigned int i = 0; i < numberOfNodes; i++)
 	{
 		for (unsigned int j = 0; j < numberOfNodes; j++)
@@ -43,9 +62,11 @@ void parseGraphFile(const string& file_path)
 		}
 	}
 
-	Kruskal kruskal;
+	Kruskal* kruskal = new Kruskal();
 
-	kruskal.calculateMst(nodeVertices, weights, numberOfNodes);
+	kruskal->calculateMst(nodeVertices, weights, numberOfNodes);
+
+	delete kruskal;
 
 	// Deletion of node vertices array
 	delete[] nodeVertices;
