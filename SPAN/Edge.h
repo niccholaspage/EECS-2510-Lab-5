2901bf7702702a4abcdbafa86a6efb4a6f21aa9e
niccholaspage <niccholaspage@gmail.com>
2020-05-02 18:17:51 -0400

    Add comments to Edge.h

--- SPAN/Edge.h
+++ SPAN/Edge.h
@@ -1,7 +1,20 @@
+//==============================================================================================
+// Edge.h
+//
+// This header file simply defines an edge, which connects two vertices together with a weight.
+// This struct is inside its own file because it is shared between both the Prim and Kruskal
+// algorithms.
+//
+// Author:     Nicholas Nassar, University of Toledo
+// Class:      EECS 2510-001 Non-Linear Data Structures, Spring 2020
+// Instructor: Dr.Thomas
+// Date:       May 4, 2020
+// Copyright:  Copyright 2020 by Nicholas Nassar. All rights reserved.
+
 #pragma once
 struct edge
 {
-	unsigned int u;
-	unsigned int v;
-	double weight;
+	unsigned int u;	// The index of the vertice representing the first vertex of the edge.
+	unsigned int v;	// The index of the vertice representing the second vertex of the edge.
+	double weight;	// The weight between the vertices u and v.
 };
